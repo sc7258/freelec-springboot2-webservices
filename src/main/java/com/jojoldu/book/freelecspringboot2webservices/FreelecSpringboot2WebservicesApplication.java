f95711345bb5fc5c73b0b8f81a25e7ce2387d2ce
Sungchul.Shin <sc7258@gmail.com>
2021-03-29 20:09:01 +0900

    initial

---
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservices/FreelecSpringboot2WebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringboot2webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringboot2WebservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelecSpringboot2WebservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojoldu/book/freelecspringboot2webservices/FreelecSpringboot2WebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringboot2webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2WebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
